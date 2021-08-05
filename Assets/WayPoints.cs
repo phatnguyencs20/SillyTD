40c733905aa473c2b29c02ed52e540cd980f97e1
NguyenGiaThuy <nguyengiathuy1998@gmail.com>
2021-08-06 01:19:12 +0700

    Reupdate repo

---
+++ Assets/WayPoints.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPoints : MonoBehaviour
+{
+    static public Transform[] wayPoints;
+
+    private void Awake()
+    {
+        wayPoints = new Transform[transform.childCount];
+        for (int i = 0; i < wayPoints.Length; i++)
+        {
+            wayPoints[i] = transform.GetChild(i);
+        }
+    }
+}
